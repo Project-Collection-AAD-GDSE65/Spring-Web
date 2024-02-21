ffc2a5cb97ec041c79e62a9d27893dcc4ccf3e77
ThanuraSilva <thanurasilva1989@gmail.com>
2024-02-21 09:42:06 +0530

    Add Teacher controller and test ? apping

---
+++ src/main/java/lk/ijse/gdse/aad/jakarthawebmvc/controller/Teacher.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.aad.jakarthawebmvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teacher?")
+public class Teacher {
+    @GetMapping
+    void getTeachersData(){
+        System.out.println("Get Teachers Data");
+    }
+}
