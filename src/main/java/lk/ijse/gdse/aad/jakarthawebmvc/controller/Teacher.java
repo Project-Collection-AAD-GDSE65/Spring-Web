4d419cb0f91a3b8dba186a70f44834e3ac3c0dca
ThanuraSilva <thanurasilva1989@gmail.com>
2024-02-21 10:20:18 +0530

    Test Path Variable/s

--- src/main/java/lk/ijse/gdse/aad/jakarthawebmvc/controller/Teacher.java
+++ src/main/java/lk/ijse/gdse/aad/jakarthawebmvc/controller/Teacher.java
@@ -1,14 +1,20 @@
 package lk.ijse.gdse.aad.jakarthawebmvc.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/teacher/**")
+@RequestMapping("/teacher")
 public class Teacher {
     @GetMapping
     void getTeachersData(){
         System.out.println("Get Teachers Data");
+    }
+    @PostMapping("/{category}")
+    void pickVariable(@PathVariable ("category") String category){
+        System.out.println("Picked variable: "+category);
+    }
+    @PostMapping("/{category}/{level}")
+    void pickVariables(@PathVariable ("category") String category,@PathVariable ("level") int level){
+        System.out.println("Picked variable: "+category +"and level is: "+level);
     }
 }
