280654e7a16e4c829f9865e9d1aa7e92ad25871a
ThanuraSilva <thanurasilva1989@gmail.com>
2024-02-21 14:32:15 +0530

    JSON handle with ResponseEntity

---
+++ src/main/java/lk/ijse/gdse/aad/jakarthawebmvc/controller/Student.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.aad.jakarthawebmvc.controller;
+
+import lk.ijse.gdse.aad.jakarthawebmvc.model.StudentDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+public class Student {
+    @GetMapping
+    String healthTest(){
+        return "Hello Student";
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<StudentDTO> checkConsume(@RequestBody StudentDTO student){
+        System.out.println(student);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(student);
+    }
+}
